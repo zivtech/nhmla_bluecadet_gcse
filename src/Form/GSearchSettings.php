0e48edfe24976fc99bed3ba4ef3038f4b8b5e605
Pete Inge <pete.inge@gmail.com>
2017-11-02 14:00:23 -0400

    Add settings form

---
+++ src/Form/GSearchSettings.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\bluecadet_gcse\Form;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Form\FormBase;
+
+/**
+ * Configure Paragraph examples to upload images per para bundle.
+ */
+class GSearchSettings extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'google_custom_search_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $settings = \Drupal::state()->get('bluecadet_gcse.settings', ['gcse_id' => '', 'gcse_path' => '']);
+
+    $form['settings']['#tree'] = TRUE;
+
+    $form['settings']['gcse_id'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('GCSE ID'),
+      '#default_value' => $settings['gcse_id'],
+      '#description' => $this->t('Enter the ID from your Google Custom Search Engine instance.'),
+    ];
+
+    $form['settings']['gcse_path'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Search page path'),
+      '#default_value' => $settings['gcse_path'],
+      '#description' => $this->t('Enter the path for the search page. Please include preceeding slash but not the trailing slash.'),
+    ];
+
+    $form['actions'] = array('#type' => 'actions');
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+    );
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+
+    $settings = $values['settings'];
+    \Drupal::state()->set('bluecadet_gcse.settings', $settings);
+  }
+}
