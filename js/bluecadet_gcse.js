fcbe4ce539260f457952252d382b43cc8f5af544
Pete Inge <pete.inge@gmail.com>
2019-02-13 10:14:10 -0500

    Tweaks

--- js/bluecadet_gcse.js
+++ js/bluecadet_gcse.js
@@ -1,9 +1,9 @@
-(function () {
+(function (drupalSettings) {
   var cx = drupalSettings.gsearch.gcse_id;
-  var gcse = document.createElement('script');
-  gcse.type = 'text/javascript';
+  var gcse = document.createElement("script");
+  gcse.type = "text/javascript";
   gcse.async = true;
-  gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
+  gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
+  var s = document.getElementsByTagName("script")[0];
   s.parentNode.insertBefore(gcse, s);
-})();
+})(drupalSettings);
